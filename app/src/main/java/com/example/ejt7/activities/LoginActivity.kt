4c30354584b21c30abe28f9c759bbd2747e8ce5f
CarlosJavier <carlos.valenzuela.j92@gmail.com>
2024-11-22 17:20:22 +0100

    Commit practica tema 9 - kotlin login

--- app/src/main/java/com/example/ejt7/activities/LoginActivity.kt
+++ app/src/main/java/com/example/ejt7/activities/LoginActivity.kt
@@ -1,7 +1,11 @@
 package com.example.ejt7.activities
 
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -29,7 +33,68 @@ class LoginActivity : AppCompatActivity() {
         }
 
         preferences = getSharedPreferences("Film Pro Manager", MODE_PRIVATE)
+        setValues()
+        binding.btnLogin.setOnClickListener {
+            val email = binding.etEmail.editText.toString()
+            val password = binding.etPassword.editText.toString()
+            if(login(email, password)) goToMain()
+            savePreferences(email, password)
+        }
 
+    }
 
+    private fun setValues(){
+        val email = preferences?.getString("email", "")
+        val password = preferences?.getString("password", "")
+        val remember = preferences?.getBoolean("remember", false)
+        if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)){
+            binding.etEmail.editText?.setText(email)
+            binding.etPassword.editText?.setText(password)
+            if (remember != null) {
+                binding.switch1.isChecked = remember
+            }
+        }
+    }
+    private fun savePreferences(email: String, password: String){
+        val edit = preferences?.edit()
+        if(binding.switch1.isChecked){
+            edit?.putString("email", email)
+            edit?.putString("password", password)
+            edit?.putBoolean("remember", true)
+            edit?.apply()
+        }else{
+            edit?.clear()
+            edit?.putBoolean("remember", false)
+            edit?.apply()
+        }
+    }
+
+    private fun eMailValidate(email: String):Boolean{
+        return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+    private fun passwordValidate(password: String):Boolean{
+        return !TextUtils.isEmpty(password) && password.length>7
+    }
+
+    private fun login(email:String, password: String): Boolean{
+        var validate = false
+        if (!eMailValidate(email)){
+            Toast.makeText(this,
+                "Invalid e-mail. Enter a correct e-mail",
+                Toast.LENGTH_SHORT).show()
+        }else if (!passwordValidate(password)){
+            Toast.makeText(this,
+                "Invalid password. The e-mail must be at least 8 character",
+                Toast.LENGTH_SHORT).show()
+        }else {
+            validate = true
+        }
+        return validate
+    }
+
+    private fun goToMain(){
+        val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
     }
 }
