40662d1b3d5fede27f46720e8dde13473ec6bc63
CarlosJavier <carlos.valenzuela.j92@gmail.com>
2024-11-28 21:05:20 +0100

    Commit practica tema 10 - OpenStreetMaps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.glide)
+    implementation(libs.osmdroid.android)
     implementation(libs.androidx.swiperefreshlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
